c534374a8049b5c53221ae7f273fda62057a2909
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-09-22 03:13:20 -0300

    Criado controller e list

---
+++ src/main/java/webdemo/controllers/ProdutosController.java
@@ -0,0 +1,28 @@
+package webdemo.controllers;
+
+import webdemo.entidades.Produtos;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class ProdutosController extends Controller{
+    public ProdutosController(HttpServletRequest req, HttpServletResponse resp){
+        super(req, resp);
+    }
+    @Override
+    public void list() throws ServletException, IOException {
+        try{
+        List<Produtos> produtos = Produtos.Todos();
+        getRequest().setAttribute("Produtos", produtos);
+        getRequest().getRequestDispatcher("/WEB-INF/templates/produtos/list.jsp").forward(getRequest(), getResponse());
+        }catch (NullPointerException ex){
+            getRequest().getRequestDispatcher("/WEB-INF/templates/produtos/list.jsp").forward(getRequest(), getResponse());
+        }
+
+
+    }
+
+}
